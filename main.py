7a23148e218305457086483e88881b74071e5aba
asbj2903 <asbj2903@edu.ucl.dk>
2022-05-10 21:15:16 +0200

    initial UI

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.utils import platform
 
-
-
-class MainWidget(Widget):
+class MyGrid(Widget):
     pass
 
-
+class BoxLayoutHangMan(BoxLayout):
+    pass
 
 class HangManApp(App):
-    pass
- 
+    def build(self):
+        
+        #Image widget
+        #self.window.add_widget(Image(source="hangman.png"))
+        #self.greeting = Label(text="Will you hang?")
+        #self.window.add_widget(self.greeting)
+
+        #self.user = TextInput(multiline=False)
+        #self.window.add_widget(self.user)
+
+
+        return BoxLayoutHangMan()
 HangManApp().run()
